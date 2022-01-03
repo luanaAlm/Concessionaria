658f09bf7dee0e5b983bad152c336754699c3c53
Luana Rodirgues <luanarodrigues3211@gmail.com>
2022-01-03 16:40:50 -0300

    Criação e insercao dos arquivos estaticos

--- servico/urls.py
+++ servico/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include, include
+# imagens
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
