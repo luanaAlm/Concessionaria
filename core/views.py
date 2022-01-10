0e565b037aebaa9678b9730bc404a7e236fd1f9b
Luana Rodirgues <luanarodrigues3211@gmail.com>
2022-01-10 17:40:52 -0300

    Criação dos seminovos

--- core/views.py
+++ core/views.py
@@ -6,9 +6,10 @@ def index(request):
     bannerPrincipal = Banner1.objects.all()
     bannersSecundarios = Banner2.objects.all()
     destaques = Carros.objects.filter(categoria="Destaques")
-
+    seminovos = Carros.objects.filter(categoria="Seminovos")
     return render(request, 'index.html', {
         "bannerPrincipal": bannerPrincipal,
         "bannersSecundarios": bannersSecundarios,
         "destaques": destaques,
+        "seminovos": seminovos,
     })
