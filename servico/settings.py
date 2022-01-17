1fafb80024650eee293b4c54a33748f6cbc8b7be
Luana Rodirgues <luanarodrigues3211@gmail.com>
2022-01-17 11:16:40 -0300

    Modificaç~]ao do banco de dados

--- servico/settings.py
+++ servico/settings.py
@@ -52,8 +52,12 @@ WSGI_APPLICATION = 'servico.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config('NAME'),
+        "USER": config('USER'),
+        "PASSWORD": config('PASSWORD'),
+        "HOST": config('HOST'),
+        "PORT": "5432",
     }
 }
 
