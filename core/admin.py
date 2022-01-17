3094f1d6b6b7ab861f7ebbd604d6ea28c3dd85d3
Luana Rodirgues <luanarodrigues3211@gmail.com>
2022-01-17 15:30:20 -0300

    Adicionando os arquivos no heroku

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import *
 
+
+@admin.register(Carros)
+class CarroAdmin(admin.ModelAdmin):
+    list_display = ('marca', 'Modelo', 'AnoInicial',
+                    'AnoFinal', 'porta', 'combustivel', 'finalPlaca', 'cor', 'valor')
+    search_fields = ('marca', 'Modelo', 'AnoInicial',
+                     'AnoFinal', 'porta', 'combustivel', 'finalPlaca', 'cor', 'valor')
+    list_filter = ('marca', 'Modelo', 'AnoInicial',
+                   'AnoFinal', 'porta', 'combustivel', 'finalPlaca', 'cor', 'valor',)
+
+
 admin.site.register(Cliente)
 admin.site.register(Banner1)
 admin.site.register(Banner2)
-admin.site.register(Carros)
 admin.site.register(Depoimento)
